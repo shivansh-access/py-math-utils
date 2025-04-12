780ae43ae6a982dafc9fc5c35bfa700e59a242f2
shivansh-access <shivanshaccess@gmail.com>
2025-04-12 13:49:07 +0530

    Initial commit: Added table generator

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from table.table import print_table
+
+if __name__ == "__main__":
+    print("Welcome to the multiplication table generator!")
+    print("Print table of 5:")
+    print_table(5, 10)
