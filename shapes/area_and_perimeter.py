577706a84b30d60024f37dbec36ca753b1db3a06
shivansh-access <shivanshaccess@gmail.com>
2025-04-17 21:04:50 +0530

    Added function to calculate area/perimeter of a triangle

--- shapes/area_and_perimeter.py
+++ shapes/area_and_perimeter.py
@@ -12,3 +12,8 @@ def area_and_circumference_of_circle(radius):
     print(f"The area of given circle of radius {radius}: {round(3.14 * radius * radius, 2 )}")
     print(f"The circumference of the given circle of radius {radius}: {round(2 * 3.14 * radius, 2)}")
 
+def area_and_perimeter_of_triangle(base, height, side1, side2):
+    print(f"The area of given triangle of base {base} and height {height}: {round(0.5 * base * height, 2)}")
+    print(f"The perimeter of the given triangle of base {base}, side1 {side1} and side2 {side2}: {base + side1 + side2}")
+
+ 
