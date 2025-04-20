924279856d730e623766e7400745777157cf55b1
shivansh-access <shivanshaccess@gmail.com>
2025-04-20 13:01:04 +0530

    Fixing project structure

--- math_utils/number_checker.py
+++ math_utils/number_checker.py
@@ -1,10 +1,7 @@
-def tell_the_diffrence_between_odd_and_even(number):
+def if_number_is_odd_or_even(number):
     remainder = number % 2
     if remainder == 0:
         return f"{number} is even"
     else:
         return f"{number} is odd"
 
-
-    
- 
--- tests/math_utils/test_number_checker.py
+++ tests/math_utils/test_number_checker.py
@@ -6,11 +6,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 
-from numbers.number_checker import (
-    if_number_is_odd_or_even
-)
+from math_utils.number_checker import if_number_is_odd_or_even
+
+
 
 class TestNumberCheckerFunctions(unittest.TestCase):
+
     def test_if_number_is_odd_or_even(self):
         # Test with an even number
         result = if_number_is_odd_or_even(4)
@@ -18,8 +19,13 @@ class TestNumberCheckerFunctions(unittest.TestCase):
 
         # Test with an odd number
         result = if_number_is_odd_or_even(5)
-        self.assertEqual(result, "Odd")
+        self.assertEqual(result, "5 is odd")
 
         # Test with zero
         result = if_number_is_odd_or_even(0)
-        self.assertEqual(result, "Even")
+        self.assertEqual(result, "0 is even")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
