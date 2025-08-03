c34589fafac9f5972af5405e56d0203435b1733c
shivansh-access <shivanshaccess@gmail.com>
2025-08-03 16:05:51 +0530

    Added airthmetic operations

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from math_utils.table import print_table
+from math_utils.airthmetic_operations import sum_of_two_numbers
 from geometry_utils.area_and_perimeter import *
-
 
 if __name__ == "__main__":
     print("-- Welcome to the multiplication table generator! --")
     print_table(7, 10)
-    print()
+    s = sum_of_two_numbers(4, 10)
+    print(f"-- Printing sum of two number: 4 and 10 is {s}")
+
---
+++ math_utils/airthmetic_operations.py
@@ -0,0 +1,21 @@
+def sum_of_two_numbers(num1, num2):
+    # print("Printing the sum of of numbers " + str(num1) + " and " + str(num2))
+    print(f"Returning the sum of of numbers {num1} and {num2}")
+    return num1 + num2
+
+
+def print_name_and_age(name, age):
+    print("My name is " + name + ". My age is " + str(age) + ".")
+    print(f"My name is {name}. My age is {age}")
+
+
+def printcommaseperatednumbers(num1, num2, num3):
+    print(f"num1, num2, num3")
+    
+    
+
+#print_name_and_age("Shivansh", 10)
+#print_name_and_age("Shubhi", 11)
+printcommaseperatednumbers(4 , 5 , 6)
+
+
